e2011f0cfd43dd6ef108f9c5ea636c1aaf84f7c6
git-joi <joiashlei@gmail.com>
2016-06-18 18:18:53 -0500

    Started mongodb stuff in gecko.js

--- geckos.js
+++ geckos.js
@@ -1,3 +1,46 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/geckotracker");
+
+var geckoSchema = new mongoose.Schema({
+    name		: String,
+    uniqueID 	: Number,
+    status		: String,
+    sex         : String,
+    morph		: String,
+    purchaseDate: Date,
+    birthDate	: Date,
+    parents 	: [String],
+    weights		: {
+        date    : Date,
+        weight	: Number
+    },
+    shedded : {
+         date   : Date
+    },
+    laid        : {
+        date	: Date,
+        viable	: Number,
+        slugs	: Number
+    },
+    gravid      : {
+        isGravid: Boolean,
+        date	: Date
+    },
+    copulated	: {
+        date    : Date,
+        partner : UniqueID
+    },
+    incubated	: {
+        startDate: Date,
+        endDate	: Date,
+        temp    : Number
+    }
+});
+
+var Gecko = mongoose.model("Gecko", geckoSchema);
+
+//add new gecko to DB for testing purposes
+
 
 
 exports.getGeckos = function() {
