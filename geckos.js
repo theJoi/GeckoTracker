a914a61e3e530344fd9ea73b5f8f3e6f21814923
git-joi <joiashlei@gmail.com>
2016-06-19 00:19:10 -0500

    Filled in getGeckos method

--- geckos.js
+++ geckos.js
@@ -5,8 +5,8 @@ var geckoSchema = new mongoose.Schema({
     name		: String,         // Name of gecko
     uniqueID 	: Number,         // External ID number
     status		: String,         // options are {normal/gravid/egg/sold/dead}
-    sex         : String,         // Female/ Male/ Other
-    morph		: String,         // Gecko's morph type
+    sex         : String,         // Female/ Male/ Unknown
+    /*morph		: String,         // Gecko's morph type
     purchaseDate: Date,           // Date gecko was purchased, n/a if not purchased
     birthDate	: Date,           // Birthdate of gecko
     parents 	: [               // Reference to parents
@@ -40,17 +40,40 @@ var geckoSchema = new mongoose.Schema({
         startDate: Date,
         endDate	: Date,
         temp    : Number
-    }
+    }*/
 });
 
 var Gecko = mongoose.model("Gecko", geckoSchema);
 
 //add new gecko to DB for testing purposes
+Gecko.create({
+    name		: "Test Gecko #1",
+    uniqueID 	: 2,
+    status		: "normal",
+    sex         : "male"
+}, function(err, newGecko){
+    if(err){
+        console.log("Error occured. Unable to add new gecko to DB:");
+        console.log(err);
+    } else {
+        console.log("Successfully added the following gecko to DB:");
+        console.log(cat);
+    }
+});
 
-
-
+// Returns all geckos in DB
 exports.getGeckos = function() {
-    return [];
+    Gecko.find({}, function(err, geckos){
+        if(err){
+            console.log("Unable to retrieve list of geckos from database:");
+            console.log(err);
+        } else {
+            console.log("Successfully retrieved list of geckos");
+            console.log(geckos.name);
+            return geckos.name;
+        }
+    });
+    return -1;
 }
 
 exports.addGecko = function(props) {
