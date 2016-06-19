15c25fbbbc3b8296df6c3d2b73b07ca55607292f
git-joi <joiashlei@gmail.com>
2016-06-18 19:51:18 -0500

    Modified gecko schema

--- geckos.js
+++ geckos.js
@@ -2,35 +2,41 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/geckotracker");
 
 var geckoSchema = new mongoose.Schema({
-    name		: String,
-    uniqueID 	: Number,
-    status		: String,
-    sex         : String,
-    morph		: String,
-    purchaseDate: Date,
-    birthDate	: Date,
-    parents 	: [String],
-    weights		: {
+    name		: String,         // Name of gecko
+    uniqueID 	: Number,         // External ID number
+    status		: String,         // options are {normal/gravid/egg/sold/dead}
+    sex         : String,         // Female/ Male/ Other
+    morph		: String,         // Gecko's morph type
+    purchaseDate: Date,           // Date gecko was purchased, n/a if not purchased
+    birthDate	: Date,           // Birthdate of gecko
+    parents 	: [               // Reference to parents
+        type    : mongoose.Schema.Type.objectId,
+        ref     : gecko
+    ],
+    weights		: {                // Weight and date weighed
         date    : Date,
         weight	: Number
     },
-    shedded : {
+    shedded : {                     // Date shedded
          date   : Date
     },
-    laid        : {
+    laid        : {                 // Date gecko laid eggs and number of viable eggs and slugs
         date	: Date,
         viable	: Number,
         slugs	: Number
     },
-    gravid      : {
+    gravid      : {                 // Date gecko became gravid or not gravid
         isGravid: Boolean,
         date	: Date
     },
-    copulated	: {
+    copulated	: {                  // Date gecko copulated and with whom
         date    : Date,
-        partner : UniqueID
+        partner : {
+            type: mongoose.Schema.Type.objectId,
+            ref : gecko
+        }
     },
-    incubated	: {
+    incubated	: {                   // Date egg was incubated and temp
         startDate: Date,
         endDate	: Date,
         temp    : Number
@@ -48,5 +54,5 @@ exports.getGeckos = function() {
 }
 
 exports.addGecko = function(props) {
-    
+
 }
