2ae3d88d406cc856cffa46e162ad0da3db5d570b
git-joi <joiashlei@gmail.com>
2016-06-21 09:46:59 -0500

    added simple grid system; added more info (and geckos) to list for index.htm

--- public/app.js
+++ public/app.js
@@ -10,14 +10,33 @@ angular.module('geckoTracker', [])
 			morph: 'Lavender Jungle Bandit',
 			birthdate: new Date('5/1/2015'),
 			weight: 57
-		},
-		{
+		},{
 			name: "Sabetha",
 			sex: 'female',
 			status: 'gravid',
 			morph: 'Lavender Jungle Bandit',
 			birthdate: new Date('5/1/2015'),
 			weight: 63
+		},{
+			name: "Eggbert",
+			sex: 'unknown',
+			status: 'egg',
+			morph: 'unknown',
+			weight: 20
+		},{
+			name: "Locke Jr.",
+			sex: 'male',
+			status: 'egg',
+			morph: 'Lavender Jungle Bandit',
+			birthdate: 'unknown',
+			weight: 21
+        },{
+			name: "Mystery Girl",
+			sex: 'female',
+			status: 'normal',
+			morph: 'unknown',
+			birthdate: new Date('6/21/2016'),
+			weight: 29
 		}
 	];
 });
