ebfa60d59aeafc0e9a38d783e2a7c6d83e5a0e10
gitJoi <joiashlei@gmail.com>
2016-06-25 12:48:09 -0400

    Revert "Merging add-gecko-form with master branch"
    This reverts commit 1dd9f5905a07761ce1e14e996e5c088fdbe8c3f0.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,16 @@
-/*jshint globals: true, undef: true, browser: false, node: true*/
+/*jshint globals: true, undef: true, browser: true, node: true*/
 /* globals angular */
 
-angular.module('geckoTracker', []).controller('GeckoListController', function($scope, $http){
-        $scope.geckos = [];
-        $scope.isLoaded = false;
+angular.module('geckoTracker', []).controller('GeckoListController', function ($scope, $http) {
+    $scope.geckos = [];
+    $scope.isLoaded = false;
 
+    $scope.refreshGeckos = function() {
         $http({
             method: 'GET',
             url: '/geckos'
         }).then(function success(response) {
-            if(response.data.error) {
+            if (response.data.error) {
                 console.log("Server side error! " + response.data.error);
                 return;
             }
@@ -18,45 +19,38 @@ angular.module('geckoTracker', []).controller('GeckoListController', function($s
         }, function error(response) {
             console.log("Error occurred: unable to get list of geckos " + response);
         });
+    };
+    $scope.refreshGeckos();
+}).controller('AddGeckoController', function ($scope, $http) {
+    $scope.validationMsg = "";
+    $scope.form = {
+        name: "",
+        uniqueID: "",
+        status: "unknown",
+        sex: "unknown",
+        morph: "",
+        purchaseDate: "",
+        birthDate: "",
+        location: ""
+    };
+
+
+    $scope.submitForm = function () {
+        console.log("submit form called");
+         if(($scope.form.status === "dead" || $scope.form.status === "sold") && $scope.form.location === ""){
+            $scope.form = "not applicable";
+       }
+        $http({
+            method: 'POST',
+            url: '/geckos',
+            data: $scope.form
+        }).then(function success(response) {
+            $scope.geckos.push(response.data);
+            $scope.validationMsg = "The gecko named " + $scope.form.name + " has successfully been added.";
+            $scope.refreshGeckos();
+            $scope.addGeckoForm.$setPristine();
+        }, function error(response) {
+             $scope.validationMsg = "Uh oh. Error occured, gecko not added. Please try again.";
+        });
+    };
 });
-/*
-.controller('GeckoListController', function($scope) {
-	$scope.geckos = [
-		{
-			name: "Locke",
-			sex: 'male',
-			status: 'normal',
-			morph: 'Lavender Jungle Bandit',
-			birthdate: new Date('5/1/2015'),
-			weight: 57
-		},{
-			name: "Sabetha",
-			sex: 'female',
-			status: 'gravid',
-			morph: 'Lavender Jungle Bandit',
-			birthdate: new Date('5/1/2015'),
-			weight: 63
-		},{
-			name: "Eggbert",
-			sex: 'unknown',
-			status: 'egg',
-			morph: 'unknown',
-			weight: 20
-		},{
-			name: "Locke Jr.",
-			sex: 'male',
-			status: 'egg',
-			morph: 'Lavender Jungle Bandit',
-			birthdate: 'unknown',
-			weight: 21
-        },{
-			name: "Mystery Girl",
-			sex: 'female',
-			status: 'normal',
-			morph: 'unknown',
-			birthdate: new Date('6/21/2016'),
-			weight: 29
-		}
-	];
-});
-*/
