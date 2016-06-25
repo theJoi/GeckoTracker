28ab4c037fbc4b6e4b8de46bebcd4d9816a81a39
git-joi <joiashlei@gmail.com>
2016-06-25 18:19:32 -0500

    added call dropCollection method

--- app/tools/populate-database.js
+++ app/tools/populate-database.js
@@ -66,11 +66,13 @@ exports.propDat = function (callback) {
         location: "incubator #2"
 }];
 
-    newGeckos.forEach(function (gecko) {
-        // console.log(gecko);
+     // Add geckos to database
+     newGeckos.forEach(function (gecko) {
         g.addGecko(gecko, function () {});
     });
     console.log('Databse population done.');
 };
 
-//g.init(null, exports.propDat);
+
+g.dropCollection(function () {});   // Empty collection first
+g.init(null, exports.propDat);
