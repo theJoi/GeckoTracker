72de7c63085c1189e30f025b1cfdce329171adfe
Kenneth Fitch <kfitch@gmail.com>
2016-06-29 01:09:13 +0000

    Stubbed out new tests

--- test/gecko-test.js
+++ test/gecko-test.js
@@ -1,11 +1,12 @@
 var assert = require('chai').assert;
-var geckos = require('../geckos.js');
+var geckos = require('../app/gecko.js');
 var mongoose = require('mongoose');
 
 // Change the database
-//geckos.init('mongodb://localhost/geckotracker_test', function(err) {
-geckos.init(null, function(err) {
+geckos.init('mongodb://localhost/geckotracker_test', function(err) {
+
 	describe('geckos', function() {
+
 		describe('getGeckos()', function() {
 			before(function() {
 				mongoose.connection.db.dropDatabase();
@@ -75,10 +76,66 @@ geckos.init(null, function(err) {
 				});
 			});
 		});
+		
+		describe('getGecko()', function() {
+			it('should exit', function() {
+				assert.isDefined(geckos.getGecko);
+			});
+			it('should accept an _id and return the proper gecko document as json', function(done) {
+				// Add a test gecko
+				geckos.addGecko({
+					'id': '16M0001',
+					'name': 'Roger',
+					'sex': 'Male',
+					'birthday': Date('6/22/2016'),
+					'status': 'normal'
+				}, function(err, newGecko) {
+					assert.isNull(err);
+					geckos.getGecko(newGecko._id, function(err, gecko) {
+						assert.isNull(err);
+						assert.isObject(gecko);
+						assert.isEqual(newGecko._id, gecko._id);
+						done();
+					});
+				});
+			});
+			it('should accept an _id and return an error if the gecko record is missing', function(done) {
+				geckos.getGecko('1234', function(err, gecko) {
+					assert.isNotNull(err);
+					done();
+				});
+			});
+		});
 
 		describe('removeGecko()', function() {
 			it('should exist', function() {
 				assert.isDefined(geckos.removeGecko);
+			});
+			it('should accept an _id and delete that document', function() {
+			});
+		});
+		
+		describe('updateGecko()', function() {
+			it('should exist', function() {
+				assert.isDefined(geckos.updateGecko);
+			});
+		});
+
+		describe('addGeckoEvent()', function() {
+			it('should exist', function() {
+				assert.isDefined(geckos.addGeckoEvent);
+			});
+		});
+
+		describe('updateGeckoEvent()', function() {
+			it('should exist', function() {
+				assert.isDefined(geckos.updateGeckoEvent);
+			});
+		});
+
+		describe('removeGeckoEvent()', function() {
+			it('should exist', function() {
+				assert.isDefined(geckos.removeGeckoEvent);
 			});
 		});
 	});
