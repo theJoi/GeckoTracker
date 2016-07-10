118ae2c651cac41229b616e0534434516b167390
Kenneth Fitch <kfitch@gmail.com>
2016-07-10 07:56:42 +0000

    Fixed and added tests for event functions, made some updates to the populate-database.js tool

--- app/tools/populate-database.js
+++ app/tools/populate-database.js
@@ -8,82 +8,115 @@
 | Created June 2016 by Joi W.
 |__________________________________________________________________________
 */
-var g = require("../gecko.js");
+var geckos = require("../gecko.js");
 
-exports.propDat = function (callback) {
+function addTestGeckos(callback) {
+	var newGeckos = [{
+		userId: '15001',
+		name: "Locke",
+		stage: 'adult',
+		status: 'normal',
+		sex: 'male',
+		morph: 'Lavender Jungle Bandit',
+		location: "bin #A-1",
+		currentWeight: 62,
+		notes: "Some sample notes.",
+		mother: {},
+		father: {},
+		dates: {
+			hatched: new Date('5/1/2015')
+		}
+    }, {
+		userId: '15002',
+		name: "Sabetha",
+		stage: 'adult',
+		status: 'normal',
+		sex: 'female',
+		morph: 'Lavender Jungle Bandit',
+		location: "bin #A-2",
+		currentWeight: 64,
+		notes: "Some sample notes.",
+		mother: {},
+		father: {},
+		dates: {
+			hatched: new Date('5/1/2015')
+		}
+    }, {
+		userId: '15002',
+		name: 'Britney',
+		stage: 'hatchling',
+		status: 'normal',
+		sex: 'female',
+		morph: 'Lavender Jungle Bandit',
+		location: "bin #A-3",
+		currentWeight: 44,
+		notes: "Some sample notes.",
+		mother: {
+			name: "Sabetha",
+			userId: "15002"
+		},
+		father: {
+			name: "Locke",
+			userId: "15001"
+		},
+		dates: {
+			hatched: new Date('6/11/2016')
+		}
+	}, {
+		name: "Test subject #211",
+		status: "normal",
+		sex: "unknown",
+		morph: "mystery morph",
+		location: "bin #2",
+		dateLaid: new Date("12/15/2015")
+    }, {
+		name: "Wee Baby",
+		status: "egg",
+		sex: "unknown",
+		morph: "unknown",
+		location: "bin #3",
+		dateLaid: new Date("12/15/2015")
+    }, {
+		name: "Eggbert",
+		sex: 'unknown',
+		status: 'egg',
+		morph: 'unknown',
+		location: "incubator #1",
+		dateLaid: new Date("12/15/2015")
+    }, {
+		name: "Locke Jr.",
+		sex: 'male',
+		status: 'egg',
+		morph: 'Lavender Jungle Bandit',
+		birthdate: 'unknown',
+		location: "incubator #1",
+		dateLaid: new Date("12/15/2015")
+    }, {
+		name: "Mystery Girl",
+		sex: 'female',
+		status: 'normal',
+		morph: 'unknown',
+		birthdate: new Date('6/21/2016'),
+		location: "incubator #2",
+		dateLaid: new Date("12/15/2015")
+    }];
 
-    var newGeckos = [{
-        name: "Britney",
-        status: "normal",
-        sex: "female",
-        morph: "rainbow bright",
-        location: "bin #1"
-        }, {
-        name: "Test subject #211",
-        status: "normal",
-        sex: "unknown",
-        morph: "mystery morph",
-        location: "bin #2",
-        dateLaid: new Date("12/15/2015")
-  }, {
-        name: "Wee Baby",
-        status: "egg",
-        sex: "unknown",
-        morph: "unknown",
-        location: "bin #3",
-        dateLaid: new Date("12/15/2015")
-  }, {
-        name: "Locke",
-        sex: 'male',
-        status: 'normal',
-        morph: 'Lavender Jungle Bandit',
-        birthdate: new Date('5/1/2015'),
-        location: "bin #A-1",
-        dateLaid: new Date("12/15/2015")
-  }, {
-        name: "Sabetha",
-        sex: 'female',
-        status: 'gravid',
-        morph: 'Lavender Jungle Bandit',
-        birthdate: new Date('5/1/2015'),
-        location: "bin #A-2",
-        dateLaid: new Date("12/15/2015")
-  }, {
-        name: "Eggbert",
-        sex: 'unknown',
-        status: 'egg',
-        morph: 'unknown',
-        location: "incubator #1",
-        dateLaid: new Date("12/15/2015")
-  }, {
-        name: "Locke Jr.",
-        sex: 'male',
-        status: 'egg',
-        morph: 'Lavender Jungle Bandit',
-        birthdate: 'unknown',
-        location: "incubator #1",
-        dateLaid: new Date("12/15/2015")
-  }, {
-        name: "Mystery Girl",
-        sex: 'female',
-        status: 'normal',
-        morph: 'unknown',
-        birthdate: new Date('6/21/2016'),
-        location: "incubator #2",
-        dateLaid: new Date("12/15/2015")
-}];
-
-     // Add geckos to database
-     newGeckos.forEach(function (gecko) {
-        g.addGecko(gecko, function () {});
-    });
-    console.log('Databse population done.');
+	 // Add geckos to database
+    var toLoad = newGeckos.length;
+	newGeckos.forEach(function (gecko) {
+		geckos.addGecko(gecko, function () {
+            toLoad--;
+            if(toLoad == 0) {
+            	console.log('Database population done.');
+                process.exit();
+            }
+        });
+	});
 };
 
-
 // Empty collection first then run function to add geckos
-g.init(null, function () {
-     g.dropCollection(function () {
-         exports.propDat(function () {});
-     });
+geckos.init(null, function () {
+	 geckos.dropCollection(function () {
+		 addTestGeckos();
+	 });
 });
--- test/gecko-test.js
+++ test/gecko-test.js
@@ -3,269 +3,300 @@ var geckos = require('../app/gecko.js');
 var mongoose = require('mongoose');
 
 // Change the database
-geckos.init('mongodb://localhost/geckotracker_test', function(err) {
-
-	var testGeckoProperties = {
+function cloneRoger() {
+	return {
 		'id': '16M0001',
 		'name': 'Roger',
 		'sex': 'Male',
 		'birthday': Date('6/22/2016'),
 		'status': 'normal'
 	};
+}
 
-	describe('geckos', function() {
-		beforeEach(function() {
-			mongoose.connection.db.dropDatabase();
-		});
-		afterEach(function() {
-			mongoose.connection.db.dropDatabase();
-		});
+describe('geckos', function() {
+	before(function(done) {
+		geckos.init('mongodb://localhost/geckotracker_test', function(err) { done(); });
+	});
+	beforeEach(function() {
+		mongoose.connection.db.dropDatabase();
+	});
+	afterEach(function() {
+		mongoose.connection.db.dropDatabase();
+	});
 
-		describe('getGeckos()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.getGeckos);
+	describe('getGeckos()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.getGeckos);
+		});
+		it('should return an empty array initially', function(done) {
+			geckos.getGeckos(function(err, result) {
+				assert.isArray(result);
+				assert.equal(result.length, 0);
+				done(err);
 			});
-			it('should return an empty array initially', function(done) {
+		});
+	});
+
+	describe('addGecko()', function() {
+		before(function() {
+			mongoose.connection.db.dropDatabase();
+		});
+		after(function() {
+			mongoose.connection.db.dropDatabase();
+		});
+
+		it('should exist', function() {
+			assert.isDefined(geckos.addGecko);
+		});
+		it('should give no error when successfully adding a gecko', function(done) {
+			var gecko = {
+				'id': '16M0001',
+				'name': 'Roger',
+				'sex': 'Male',
+				'birthday': Date('6/22/2016'),
+				'status': 'normal'
+			};
+			geckos.addGecko(gecko, function(err) {
+				assert.isNotOk(err);
+				done();
+			});
+		});
+		it('should give an error when the argument isn\'t an object', function(done) {
+			geckos.addGecko(42, function(err) {
+				assert.isOk(err);
+				done();
+			})
+		});
+		it('should give an error when required properties are missing', function(done) {
+			var gecko = {
+				'foo': 'bar'
+			};
+			geckos.addGecko(gecko, function(err) {
+				assert.isOk(err);
+				done();
+			});
+		});
+		it('should make added geckos persistent', function(done) {
+			var gecko = {
+				'id': '16M0001',
+				'name': 'Roger',
+				'sex': 'Male',
+				'birthday': Date('6/22/2016'),
+				'status': 'normal'
+			};
+			geckos.addGecko(gecko, function(err) {
+				assert.isNull(err);
 				geckos.getGeckos(function(err, result) {
 					assert.isArray(result);
-					assert.equal(result.length, 0);
-					done(err);
+					assert.lengthOf(result, 1);
+					assert.isObject(result[0]);
+					assert.propertyVal(result[0], 'name', 'Roger');
+					done();
 				});
 			});
 		});
+	});
 	
-		describe('addGecko()', function() {
-			before(function() {
-				mongoose.connection.db.dropDatabase();
-			});
-			after(function() {
-				mongoose.connection.db.dropDatabase();
-			});
-
-			it('should exist', function() {
-				assert.isDefined(geckos.addGecko);
-			});
-			it('should give no error when successfully adding a gecko', function(done) {
-				var gecko = {
-					'id': '16M0001',
-					'name': 'Roger',
-					'sex': 'Male',
-					'birthday': Date('6/22/2016'),
-					'status': 'normal'
-				};
-				geckos.addGecko(gecko, function(err) {
-					assert.isNotOk(err);
-					done();
-				});
-			});
-			it('should give an error when the argument isn\'t an object', function(done) {
-				geckos.addGecko(42, function(err) {
-					assert.isOk(err);
-					done();
-				})
-			});
-			it('should give an error when required properties are missing', function(done) {
-				var gecko = {
-					'foo': 'bar'
-				};
-				geckos.addGecko(gecko, function(err) {
-					assert.isOk(err);
-					done();
-				});
-			});
-			it('should make added geckos persistent', function(done) {
-				var gecko = {
-					'id': '16M0001',
-					'name': 'Roger',
-					'sex': 'Male',
-					'birthday': Date('6/22/2016'),
-					'status': 'normal'
-				};
-				geckos.addGecko(gecko, function(err) {
+	describe('getGecko()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.getGecko);
+		});
+		it('should accept an _id and return the proper gecko document as json', function(done) {
+			// Add a test gecko
+			geckos.addGecko({
+				'id': '16M0001',
+				'name': 'Roger',
+				'sex': 'Male',
+				'birthday': Date('6/22/2016'),
+				'status': 'normal'
+			}, function(err, newGecko) {
+				assert.isNull(err);
+				geckos.getGecko(newGecko._id, function(err, gecko) {
+					/*
 					assert.isNull(err);
-					geckos.getGeckos(function(err, result) {
-						assert.isArray(result);
-						assert.lengthOf(result, 1);
-						assert.isObject(result[0]);
-						assert.propertyVal(result[0], 'name', 'Roger');
-						done();
-					});
+					assert.isObject(gecko);
+					console.log("!!!!!!!!!!!!", gecko);
+					assert.isEqual(newGecko._id, gecko._id);
+					*/
+					done();
 				});
 			});
 		});
-		
-		describe('getGecko()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.getGecko);
+		it('should accept an _id and return an error if the gecko record is missing', function(done) {
+			geckos.getGecko('1234', function(err, gecko) {
+				assert.isNotNull(err);
+				done();
 			});
-			it('should accept an _id and return the proper gecko document as json', function(done) {
-				// Add a test gecko
-				geckos.addGecko({
-					'id': '16M0001',
-					'name': 'Roger',
-					'sex': 'Male',
-					'birthday': Date('6/22/2016'),
-					'status': 'normal'
-				}, function(err, newGecko) {
-					assert.isNull(err);
-					geckos.getGecko(newGecko._id, function(err, gecko) {
-						/*
+		});
+	});
+
+	describe('removeGecko()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.removeGecko);
+		});
+		it('should accept an _id and delete that document', function(done) {
+			// Add a gecko
+			var gecko = {
+				'id': '16M0001',
+				'name': 'Roger',
+				'sex': 'Male',
+				'birthday': Date('6/22/2016'),
+				'status': 'normal'
+			};
+			geckos.addGecko(gecko, function(err) {
+				assert.isNull(err);
+				geckos.getGeckos(function(err, result) {
+					assert.isArray(result);
+					assert.lengthOf(result, 1);
+					assert.propertyVal(result[0], 'name', 'Roger');
+					geckos.removeGecko(result[0]._id, function(err) {
 						assert.isNull(err);
-						assert.isObject(gecko);
-						console.log("!!!!!!!!!!!!", gecko);
-						assert.isEqual(newGecko._id, gecko._id);
-						*/
-						done();
-					});
-				});
-			});
-			it('should accept an _id and return an error if the gecko record is missing', function(done) {
-				geckos.getGecko('1234', function(err, gecko) {
-					assert.isNotNull(err);
-					done();
-				});
-			});
-		});
-
-		describe('removeGecko()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.removeGecko);
-			});
-			it('should accept an _id and delete that document', function(done) {
-				// Add a gecko
-				var gecko = {
-					'id': '16M0001',
-					'name': 'Roger',
-					'sex': 'Male',
-					'birthday': Date('6/22/2016'),
-					'status': 'normal'
-				};
-				geckos.addGecko(gecko, function(err) {
-					assert.isNull(err);
-					geckos.getGeckos(function(err, result) {
-						assert.isArray(result);
-						assert.lengthOf(result, 1);
-						assert.propertyVal(result[0], 'name', 'Roger');
-						geckos.removeGecko(result[0]._id, function(err) {
-							assert.isNull(err);
-							geckos.getGeckos(function(err, result) {
-								assert.isArray(result);
-								assert.lengthOf(result, 0);
-								done();
-							});
-						});
-					});
-				});
-			});
-			it('should return an error if the _id doesn\'t exist', function(done) {
-				geckos.removeGecko("1234", function(err) {
-					assert.isNotNull(err);
-					done();
-				})
-			});
-		});
-		
-		describe('updateGecko()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.updateGecko);
-			});
-			it("should properly update a gecko\'s properties", function(done) {
-				geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
-					geckos.updateGecko(gecko._id, {status: 'dead'}, function(err, updatedGecko) {
-						assert.isNull(err);
-						assert.propertyVal(updatedGecko, '_id', gecko._id)
-						assert.property(updatedGecko, 'status');
-						assert.propertyVal(updatedGecko, 'status', 'dead');
-						done();
-					});
-				});
-			});
-			it("should give an error if the gecko can't be found (and the return gecko should be null)", function(done) {
-				geckos.updateGecko(gecko._id, {status: 'dead'}, function(err, gecko) {
-					assert.isNotNull(err);
-					assert.isNull(gecko);
-				});
-			});
-			it("should give an error if trying to update a property not defined the gecko data model", function(done) {
-				geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
-					geckos.updateGecko(gecko._id, {foo: 'bar'}, function(err, updatedGecko) {
-						assert.isNotNull(err);
-						assert.isNull(updatedGecko);
-						done();
-					});
-				});
-			});
-		});
-
-		describe('getGeckoEvents()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.addGeckoEvent);
-			});
-			it("should return an empty array if no events have been added", function(done) {
-				// Add Roger
-				geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
-					geckos.getGeckoEvents(gecko._id, function(err, events) {
-						assert.isArray(events);
-						assert.lengthOf(events, 0);
-						done();
-					});
-				});
-			});
-			it("should return all events for the given gecko", function(done) {
-				// Add Roger
-				geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
-					geckos.addGeckoEvent({type: 'weighed', value: 42}, function(err) {
-						geckos.getGeckoEvents(gecko._id, function(err, events) {
-							assert.isArray(events);
-							assert.lengthOf(events, 1);
-							assert.isObject(events[0]);
-							assert.propertyVal(events[0], 'type', 'weighed');
-							assert.propertyVal(events[0], 'value', 64);
+						geckos.getGeckos(function(err, result) {
+							assert.isArray(result);
+							assert.lengthOf(result, 0);
 							done();
 						});
 					});
 				});
 			});
 		});
-
-		describe('addGeckoEvent()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.addGeckoEvent);
-			});
-			it('should set the event date property to the current date if none is given', function(done) {
-				geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
-					geckos.addGeckoEvent({type: 'weighed', value: 42}, function(err, event) {
-						assert.isObject(event);
-						assert.property(events, 'date');
-						done();
-					});
-				});
-			});
-			it("shouldn't set the event date property to the current date if one is given", function(done) {
-				geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
-					geckos.addGeckoEvent({date: new Date('3/5/2014'), type: 'weighed', value: 42}, function(err, event) {
-						assert.isObject(event);
-						assert.property(events, 'date');
-						assert.equal(events.date.month, 3);
-						assert.equal(events.date.year, 2014);
-						assert.equal(events.date.day, 5);
-						done();
-					});
+		it('should return an error if the _id doesn\'t exist', function(done) {
+			geckos.removeGecko("1234", function(err) {
+				assert.isNotNull(err);
+				done();
+			})
+		});
+	});
+	
+	describe('updateGecko()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.updateGecko);
+		});
+		it("should properly update a gecko\'s properties", function(done) {
+			geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
+				geckos.updateGecko(gecko._id, {status: 'dead'}, function(err, updatedGecko) {
+					assert.isNull(err);
+					assert.propertyVal(updatedGecko, '_id', gecko._id)
+					assert.property(updatedGecko, 'status');
+					assert.propertyVal(updatedGecko, 'status', 'dead');
+					done();
 				});
 			});
 		});
-
-		describe('updateGeckoEvent()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.updateGeckoEvent);
+		it("should give an error if the gecko can't be found (and the return gecko should be null)", function(done) {
+			geckos.updateGecko(gecko._id, {status: 'dead'}, function(err, gecko) {
+				assert.isNotNull(err);
+				assert.isNull(gecko);
 			});
 		});
-
-		describe('removeGeckoEvent()', function() {
-			it('should exist', function() {
-				assert.isDefined(geckos.removeGeckoEvent);
+		it("should give an error if trying to update a property not defined the gecko data model", function(done) {
+			geckos.addGecko(jQuery.extend({}, roger), function(err, gecko) {
+				geckos.updateGecko(gecko._id, {foo: 'bar'}, function(err, updatedGecko) {
+					assert.isNotNull(err);
+					assert.isNull(updatedGecko);
+					done();
+				});
 			});
 		});
 	});
 
+	describe('getEvents()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.getEvents);
+		});
+		it("should return an empty array if no events have been added", function(done) {
+			geckos.addGecko(cloneRoger(), function(err, gecko) {
+				geckos.getEvents(gecko._id, function(err, events) {
+					assert.isArray(events);
+					assert.lengthOf(events, 0);
+					done();
+				});
+			});
+		});
+		it("should return all events for the given gecko", function(done) {
+			geckos.addGecko(cloneRoger(), function(err, gecko) {
+				geckos.addEvent({geckoId: gecko._id, date: new Date(), type: 'weighed', value: 42}, function(err) {
+					geckos.getEvents(gecko._id, function(err, events) {
+						assert.isArray(events);
+						assert.lengthOf(events, 1);
+						assert.isObject(events[0]);
+						assert.propertyVal(events[0], 'type', 'weighed');
+						assert.propertyVal(events[0], 'value', 64);
+						done();
+					});
+				});
+			});
+		});
+	});
+
+	describe('addEvent()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.addEvent);
+		});
+		it('should set the event date property to the current date if none is given', function(done) {
+			geckos.addGecko(cloneRoger(), function(err, gecko) {
+				geckos.addEvent({geckoId: gecko._id, type: 'weighed', value: 42}, function(err, event) {
+					assert.isObject(event);
+					assert.property(events, 'date');
+					done();
+				});
+			});
+		});
+		it("shouldn't set the event date property to the current date if one is given", function(done) {
+			geckos.addGecko(cloneRoger(), function(err, gecko) {
+				geckos.addEvent({geckoId: gecko._id, date: new Date('3/5/2014'), type: 'weighed', value: 42}, function(err, event) {
+					assert.isObject(event);
+					assert.property(events, 'date');
+					assert.equal(events.date.month, 3);
+					assert.equal(events.date.year, 2014);
+					assert.equal(events.date.day, 5);
+					done();
+				});
+			});
+		});
+	});
+
+	describe('updateEvent()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.updateEvent);
+		});
+		it('should properly update the properties of an event', function(done) {
+			geckos.addGecko(cloneRoger(), function(err, gecko) {
+				geckos.addEvent({geckoId: gecko._id, date: new Date(), type: 'weighed', value: 42}, function(err, event) {
+					assert.isObject(event);
+					assert.property(events, 'type');
+					assert.propertyVal(events, 'type', 'weighed');
+					geckos.updateEvent(event._id, {type: 'shed'}, function(err, updatedEvent) {
+						assert.isObject(updatedEvent);
+						assert.propertyVal(updatedEvent, '_id', event._id);
+						assert.property(updatedEvent, 'type');
+						assert.propertyVal(updatedEvent, 'type', 'shed');
+						done();
+					});
+				});
+			});
+		});
+	});
+
+	describe('removeEvent()', function() {
+		it('should exist', function() {
+			assert.isDefined(geckos.removeEvent);
+		});
+		it('should properly remove an event', function(done) {
+			geckos.addGecko(cloneRoger(), function(err, gecko) {
+				geckos.addEvent({geckoId: gecko._id, date: new Date(), type: 'weighed', value: 42}, function(err, event) {
+					assert.isNull(err);
+					geckos.removeEvent(event._id, function(err) {
+						assert.isNull(err);
+						geckos.getGeckoEvents(gecko._id, function(err, events) {
+							assert.isNull(err);
+							assert.isArray(events);
+							assert.lengthOf(events, 0);
+							done();
+						});
+					});
+				});
+			});
+		});
+	});
 });
